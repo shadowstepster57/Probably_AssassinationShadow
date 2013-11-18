577cb4808f71cd936a97c28491dddda0629d6667
shadowstepster57 <keenep57@me.com>
2013-11-18 15:50:48 -0600

    whoops fixin

--- rotation.lua
+++ rotation.lua
@@ -3,14 +3,33 @@
 -- Created on Nov 18th 2013 9:42 pm
 ProbablyEngine.rotation.register_custom(259, "Assassination[Shadow]", {
 
-  -- =======================================
-  --          Your Rotation Here!
-  -- =======================================
-  -- Everything between the first set of { }
-  -- in the core rotations can go here
-  -- =======================================
-  -- If you have any questions, please ask
-  -- on the ProbablyEngine Forums!
-  -- =======================================
+  -- Buffs
+  { "Deadly Poison", "!player.buff(Deadly Poison)" },
+
+  -- Interrupts
+  { "Kick", "modifier.interrupts" },
+
+  -- Cooldowns
+  { "Shadow Blades", "modifier.cooldowns" },
+  { "Vendetta", "modifier.cooldowns" },
+
+  -- Rotation
+  { "Slice and Dice", {
+    "!player.buff(Slice and Dice)",
+    "player.combopoints >= 2"
+  }},
+  { "Envenom", {
+    "player.buff(Slice and Dice).duration <= 3",
+    "player.combopoints > 0"
+  }},
+  { "Rupture", {
+    "target.debuff(Rupture).duration <= 8",
+    "player.combopoints = 5"
+  }},
+  { "Envenom", "player.combopoints = 5" },
+  { "Dispatch", "player.buff(Blindside)" },
+  { "Fan of Knives", "modifier.multitarget" },
+  { "Mutilate", "target.health > 35" },
+  { "Dispatch", "target.health < 35" },
 
 })
